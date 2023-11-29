60945fd5d93b673a765e130365734e3aed44bb4a
danielglownia <149329576+danielglownia@users.noreply.github.com>
2023-11-28 18:58:13 -0600

    add stuff

---
+++ pipeline/app.py
@@ -0,0 +1,15 @@
+from modules import data
+
+print("1. Data Retrieval:")
+
+
+print("2. Data Pre-processing:")
+
+
+print("3. Timestamping Methodology:")
+
+
+print("4. Identification of rhythmic genes:")
+
+
+print("5. Validation and Laboratory Experiments:")
