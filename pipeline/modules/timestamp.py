4be33ef4bcc9f68a7dfa99b266e67e22f525ba4a
danielglownia <149329576+danielglownia@users.noreply.github.com>
2024-01-08 23:28:39 -0600

    more r files:

--- pipeline/modules/timestamp.py
+++ pipeline/modules/timestamp.py
@@ -2,32 +2,24 @@ import subprocess
 import os
 
 def run_runCIRCUST_R(tsv_file_path):
-  """
-  Runs the "runCIRCUST.R" command line with the provided TSV file path.
-
-  Args:
-    tsv_file_path: The path to the TSV file.
-
-  Returns:
-    A tuple containing the standard output and standard error of the command as strings.
-
-  Raises:
-    FileNotFoundError: If the provided TSV file path is not found.
-  """
-
-  # Check if the provided TSV file exists
-  if not os.path.exists(tsv_file_path):
-    raise FileNotFoundError(f"File not found: {tsv_file_path}")
 
   # Build the command line
-  command = ["Rscript", "./CIRCUST-main/runCIRCUST.R", "--tsv", tsv_file_path]
+  path = os.getcwd()
+  print("Current path: ", path)
+  dir_list = os.listdir(path)
+ 
+  print("Files and directories in '", path, "' :")
+ 
+  # prints all files
+  print(dir_list)
+  cir_path = "C:\\app\\CIRCUST-main\\runCIRCUST.R"
+  print(cir_path)
+  command = ["Rscript", cir_path]
 
-  # Run the command line and capture the output and error
-  process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-  output, error = process.communicate()
+  # Run the R script and capture the output
+  output = subprocess.check_output(command)
 
-  # Decode the output and error from byte strings to text strings
-  output = output.decode("utf-8")
-  error = error.decode("utf-8")
+  # Print the output
+  print(output)
 
-  return output, error
+  return output
